5600652a3ca56d06692576b07f3fec2ce612479d
HP <HP@LAPTOP-16PQ9653>
2023-11-23 16:41:02 +0100

    Commit 3

--- Bioinformatyka strukturalna - 1.py
+++ Bioinformatyka strukturalna - 1.py
@@ -4,20 +4,20 @@ import numpy
 import pylab
 
 #Odleglosc pomiedzy weglami alfa par reszt
-def residue_distance(res_1, res_2):
+def distance(res_1, res_2):
     if "CA" in res_1 and "CA" in res_2:
         distance = res_1["CA"].coord - res_2["CA"].coord
         return numpy.sqrt(numpy.sum(distance * distance))
 
 
 #wypelnianie macierzy odleglosci wartosciami w Da
-def fill_distance_matrix(ch_1, ch_2):
-    distance_matrix = numpy.zeros((len(ch_1), len(ch_2)), numpy.single)
+def matrix(ch_1, ch_2):
+    matrix_dist = numpy.zeros((len(ch_1), len(ch_2)), numpy.single)
     for i, res_1 in enumerate(ch_1):
         for j, res_2 in enumerate(ch_2):
-            distance_matrix[i, j] = residue_distance(res_1, res_2)
+            matrix_dist[i, j] = distance(res_1, res_2)
 
-    return distance_matrix
+    return matrix_dist
 
 #Odczyt z pliku PDB
 id = "1HHB"
@@ -26,25 +26,25 @@ structure = Bio.PDB.PDBParser().get_structure(id, file)
 chain = structure[0]
 
 #Przypisywanie wartosci w macierzy odleglosci z 2 argumentami dla lancuchow
-distance_matrix = fill_distance_matrix(chain["A"], chain["B"])
+matrix_dist = matrix(chain["A"], chain["B"])
 
 #Wypelnienie mapy kontaktow zerami
-protein_contact_map = numpy.zeros((len(distance_matrix), len(distance_matrix)), numpy.single)
+map = numpy.zeros((len(matrix_dist), len(matrix_dist)), numpy.single)
 
 #Wypelnienie mapy kontaktow 1, jesli spelniony jest warunek odleglosci < 8 Da
-for i, a in enumerate(distance_matrix):
-    for j, b in enumerate(distance_matrix):
-        if distance_matrix[i][j] < 8.0:
-            protein_contact_map[i][j] = 1
+for i, a in enumerate(matrix_dist):
+    for j, b in enumerate(matrix_dist):
+        if matrix_dist[i][j] < 8.0:
+            map[i][j] = 1
 
 
 #Wykres dla mapy kontaktow
 plt.figure(figsize=(6, 6))
-plt.scatter(*numpy.where(protein_contact_map), color='red', marker='.')
+plt.scatter(*numpy.where(map), color='red', marker='.')
 plt.show()
 
 #Wykres dla macierzy odleglosci
 pylab.figure(figsize=(6, 6))
-pylab.imshow(numpy.transpose(distance_matrix))
+pylab.imshow(numpy.transpose(matrix_dist))
 pylab.colorbar()
 pylab.show()
